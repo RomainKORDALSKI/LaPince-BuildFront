6c513fbcaf8e09b3fce1e17d85e4a795d44ef1c4
RomainKORDALSKI <romain.kordalski@oclock.school>
2024-09-05 14:34:20 +0200

    front lapince

--- models/dbClientSequelize.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-export const sequelize = new Sequelize(
-  "postgres://lapince:lapince@localhost:5432/lapince2",
-  {
-    dialect: "postgres",
-    logging: false, // Optionnel : pour désactiver les logs de requêtes
-  }
-);
